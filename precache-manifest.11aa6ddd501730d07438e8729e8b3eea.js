d8293bd3cf07b3b0c299ba765ac3bf7781abc256
duc <nguyenminhduc1327@gmail.com>
2019-10-02 12:12:07 +0700

    Updates

---
+++ precache-manifest.11aa6ddd501730d07438e8729e8b3eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c740cb38641d8d0304b3308e741e977",
+    "url": "/catreactJS/index.html"
+  },
+  {
+    "revision": "ce280313d4e43b6cfc75",
+    "url": "/catreactJS/static/css/main.bcae56e8.chunk.css"
+  },
+  {
+    "revision": "790143b3b960e2de3f50",
+    "url": "/catreactJS/static/js/2.5b5af000.chunk.js"
+  },
+  {
+    "revision": "ce280313d4e43b6cfc75",
+    "url": "/catreactJS/static/js/main.6df8b33e.chunk.js"
+  },
+  {
+    "revision": "00a71cff2fea60c44cf9",
+    "url": "/catreactJS/static/js/runtime~main.bd2baf1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/catreactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
